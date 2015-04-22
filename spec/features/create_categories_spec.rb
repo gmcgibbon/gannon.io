125d313ee451565023fbe2f65a4d12d0be73e355
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2015-04-22 01:39:10 -0500

    Finished category create spec (until js revise), removed deprecated .exists? call *gasp*

--- spec/features/create_categories_spec.rb
+++ spec/features/create_categories_spec.rb
@@ -8,15 +8,16 @@ RSpec.feature 'CreateCategories', type: :feature, js: true do
 
     let!(:user) { FactoryGirl.create(:user, :admin) }
     let!(:categories) { FactoryGirl.create_list(:category, 5) }
+    let!(:article) { FactoryGirl.create(:article, :with_user, with_categories: [categories.first]) }
 
     before { login_as(user) }
 
     context 'create valid category' do
 
-      scenario 'from main page' do
+      scenario 'from root' do
 
         create_category(
-          title:   'Test Article!'
+          title:   'Test Category 1!'
         )
 
         # Does not work with prompts
@@ -25,11 +26,41 @@ RSpec.feature 'CreateCategories', type: :feature, js: true do
         # end
       end
 
+      scenario 'from article edit' do
+
+        click_link(article.title)
+        click_link('Edit')
+
+        create_category(
+          title:   'Test Category 2!'
+        )
+
+        # Does not work with prompts
+        # within(:css, '#categories .list ul') do
+        #   expect(page).to have_selector 'li', categories.count +1
+        # end
+
+      end
+
     end
 
     context 'create invalid category' do
 
-      scenario 'from main page' do
+      scenario 'from root' do
+
+        create_category(
+          title:   ''
+        )
+
+        within(:css, '#categories .list ul') do
+          expect(page).to have_selector 'li', categories.count
+        end
+      end
+
+      scenario 'from article edit' do
+
+        click_link(article.title)
+        click_link('Edit')
 
         create_category(
           title:   ''
