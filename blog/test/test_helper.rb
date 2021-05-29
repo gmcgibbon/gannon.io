00199e8cee4ba429b7497c3238f27f119b20e84c
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2021-05-29 16:18:44 -0400

    Fix tests

--- blog/test/test_helper.rb
+++ blog/test/test_helper.rb
@@ -1,13 +1,10 @@
-# Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
-
 require_relative "../../config/environment"
 require "rails/test_help"
 
+class ActiveSupport::TestCase
+  # Run tests in parallel with specified workers
+  parallelize(workers: :number_of_processors)
 
-# Load fixtures from the engine
-if ActiveSupport::TestCase.respond_to?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
-  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
-  ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
+  self.file_fixture_path = "#{Rails.root}/test/fixtures/files"
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
-ENV['RAILS_ENV'] ||= 'test'
+ENV["RAILS_ENV"] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
 
@@ -6,8 +6,7 @@ class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  self.file_fixture_path = "#{Rails.root}/test/fixtures/files"
 
   # Add more helper methods to be used by all tests here...
 end
