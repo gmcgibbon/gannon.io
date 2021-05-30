723f590dceca9a690cef10263ac5ddf9150b3520
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2021-05-30 01:42:53 -0400

    Add Blog::ApplicationModel

--- blog/lib/blog.rb
+++ blog/lib/blog.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
+require "active_model"
+
 require "blog/version"
 require "blog/engine"
--- blog/test/blog_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "test_helper"
-
-class BlogTest < ActiveSupport::TestCase
-  test "it has a version number" do
-    assert Blog::VERSION
-  end
-end
