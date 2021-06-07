4fda4b1bdc30693bb24e918ae22f3e2046e4bf66
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2021-06-06 22:18:59 -0400

    Require view component engine

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ require "action_controller/railtie"
 require "action_view/railtie"
 require "rails/test_unit/railtie"
 require "sprockets/railtie"
+require "view_component/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
