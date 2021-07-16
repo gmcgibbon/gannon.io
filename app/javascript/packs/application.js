daba2f50633488fa5c2659ae32f3e18a8ea2f6d3
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2021-07-16 17:57:46 -0400

    Learn how to tailwind

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,3 +10,31 @@ Rails.start()
 Turbolinks.start()
 
 import "stylesheets/application"
+
+const getMode = () => {
+  if ("mode" in localStorage) {
+    return localStorage.mode;
+  } else {
+    let mode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+    setMode(mode);
+
+    return mode;
+  }
+}
+
+const setMode = (mode) => {
+  localStorage.mode = mode;
+}
+
+document.addEventListener("turbolinks:load", () => {
+  document.documentElement.classList.add(getMode()); 
+
+  document.querySelector("#dark-toggle").addEventListener("click", (event) => {
+    event.preventDefault();
+    const mode = getMode();
+    const oppositeMode = mode == "dark" ? "light" : "dark";
+    document.documentElement.classList.remove(mode);
+    document.documentElement.classList.add(oppositeMode);
+    setMode(oppositeMode);
+  });
+});
