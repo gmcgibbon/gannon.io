145d755738de7fdafd2a7550dcd457d8d1b29ebb
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2021-07-25 22:21:14 -0400

    Fix engine route format matcher

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root(to: "application#index")
 
-  mount(Blog::Engine, at: :blog)
+  mount(Blog::Engine, at: :blog, path: "blog(.:format)")
 
   resources(:talks, only: %i(index))
 
