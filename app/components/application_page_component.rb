0b8aa1026cb3fdcb990d1febdaee89081ebd51fc
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2021-08-02 22:50:55 -0400

    Favicon

--- app/components/application_page_component.rb
+++ app/components/application_page_component.rb
@@ -3,4 +3,15 @@
 class ApplicationPageComponent < ApplicationComponent
   renders_one(:title)
   renders_one(:body)
+
+  private
+
+  def favicon_meta_tags
+    [
+      tag("link", rel: "icon", href: "/favicon.ico", sizes: "any"), # 32×32
+      tag("link", rel: "icon", href: "/icon.svg", type: "image/svg+xml"),
+      tag("link", rel: "apple-touch-icon", href: "/apple-touch-icon.png", type: "image/svg+xml"), # 180×180
+      tag("link", rel: "manifest", href: "/manifest.webmanifest")
+    ].join("\n").html_safe
+  end
 end
